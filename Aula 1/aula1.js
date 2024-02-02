cb3e6a864b2d35bf53990e47d8e5cc15ef5d8d64
wesleyfigueira <wesleyfigueira1993@gmail.com>
2024-02-02 14:40:23 -0300

    primeiro desafio Ada Logica de programação

---
+++ Aula 1/aula1.js
@@ -0,0 +1,34 @@
+
+ /*---------------------Desafio 1--------------------------*/
+
+let idade =prompt("Qual a sua idade ? ")
+let nacionalidade = prompt (" Qual a sua nacionalidade ")
+
+
+console.log(idade >= 16 && nacionalidade == "brasileira")
+
+
+/*---------------------Desafio 2--------------------------*/
+let salario = prompt("Digite o seu Salario: ")
+let credito = prompt("Digite o seu Score: ")
+let idade2 = prompt(" Digite a sua idade :")
+
+console.log(salario >= 5000 && credito >= 700 && idade2 >= 18);
+
+/*---------------------Desafio 3--------------------------*/
+
+
+let navegador = prompt("Digite o seu navegador : ")
+let sistemaOperacional= prompt("Digite o seu Sitema Operacional:")
+
+console.log( navegador == ["chrome","firefox"] &&  sistemaOperacional == ["windows","mac"]) 
+
+
+/*---------------------Desafio 4--------------------------*/
+
+
+let idade3 = prompt("Digite a sua idade : ")
+let compraMinima = prompt ("Digite o valor de sua compra: ")
+let newclient = prompt (" è sua primeira compra ? ")
+
+console.log(idade3 >=60  && compraMinima >= 100 && newclient =="sim")
