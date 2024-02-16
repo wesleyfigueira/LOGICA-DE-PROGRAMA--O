61796eaf182038b20fec83b145ffa7c7dae63fac
wesleyfigueira <wesleyfigueira1993@gmail.com>
2024-02-16 12:12:29 -0300

    exercicio 5

--- Aula 5/desafio5.js
+++ Aula 5/desafio5.js
@@ -21,7 +21,36 @@ Imprima se o número é primo ou não.*/
 
 
 //const sorteado1 = Math.floor(Math.random()*100)+1;
-const sorteado1 =8
-let numeroPrimo = sorteado1 % sorteado1 == 0
 
-console.log(numeroPrimo)
+
+
+
+/*Desafio 3
+- Soma de Números Pares
+● Desenvolva um programa que peça ao usuário para inserir números 
+inteiros. Use um loop while para calcular a soma dos números 
+pares inseridos pelo usuário. Pare a entrada quando o usuário 
+inserir 0 e exiba a soma total.*/
+
+let soma = 0;
+
+while (true) {
+    let numero = parseInt(prompt("Insira um número inteiro (Digite 0 para parar): "));
+    
+    
+    if (isNaN(numero)) {
+        alert("Por favor, insira um número válido.");
+        continue; 
+    }
+ 
+    if (numero === 0) {
+        break;
+    }
+    
+    if (numero % 2 === 0) {
+    
+        soma += numero;
+    }
+}
+
+alert("A soma dos números pares é: " + soma);
